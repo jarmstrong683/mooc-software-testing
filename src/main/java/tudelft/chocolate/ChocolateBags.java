b93872837ee74116cb195e98c1a711d3caf04039
mud3053 <s3163558@student.rmit.edu.au>
2020-11-21 21:33:19 +1030

    for chocolatebags inserting a check that the parameters are not negative, not tested.

--- src/main/java/tudelft/chocolate/ChocolateBags.java
+++ src/main/java/tudelft/chocolate/ChocolateBags.java
@@ -3,6 +3,8 @@ package tudelft.chocolate;
 public class ChocolateBags {
 //returns how many small chocolates were used// -ve 1 if n ot possible
     public int calculate(int small, int big, int total) {
+        if(small<0||big<0||total<0)
+            return -1;
         int maxBigBoxes = total / 5;
         int bigBoxesWeCanUse = maxBigBoxes < big ? maxBigBoxes : big;
         total -= (bigBoxesWeCanUse * 5);
